f11329bf7c4e4e06cfbfa8c69b8a9ce2601b4033
Vivek Kumar <106459759+vivekbarnaon@users.noreply.github.com>
2026-06-11 01:10:36 +0530

    rmv cnts

--- backend-java/src/main/java/com/freshtrack/util/DBConfig.java
+++ backend-java/src/main/java/com/freshtrack/util/DBConfig.java
@@ -10,14 +10,14 @@ public class DBConfig {
     private static HikariDataSource dataSource;
 
     static {
-        // .env file load karna
+
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
         String url = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : dotenv.get("DB_URL");
         String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : dotenv.get("DB_USER");
         String pass = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : dotenv.get("DB_PASSWORD");
 
-        // --- DEBUG PRINT: Crash se pehle check karna ---
+
         System.out.println("DEBUG: DB_URL found is -> " + url);
 
         if (url == null) {
@@ -32,7 +32,7 @@ public class DBConfig {
         config.setMaximumPoolSize(10);
         config.setConnectionTimeout(30000);
 
-        // Ab initialize karein
+
         dataSource = new HikariDataSource(config);
     }
 
